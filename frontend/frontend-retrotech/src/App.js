35b0fd636e2ca612d2898d41ce48e3161f230316
Guillx <guillx.gv@gmail.com>
2021-04-04 05:38:11 +0200

    frontend-resubido

--- frontend/frontend-retrotech/src/App.js
+++ frontend/frontend-retrotech/src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+import Header from "./components/pagina/Header";
+import AdminRoute from "./components/rutas/AdminRoute";
+
+import PrivateRoute from "./components/rutas/PrivateRoute";
+import PublicRoute from "./components/rutas/PublicRoute";
+import Admin from "./pages/Admin";
+import Home from "./pages/Home";
+import ListaDeCategorias from "./pages/ListaDeCategorias";
+import ListaAnunciosCategoria from "./pages/ListaAnunciosCategoria";
+import Anuncio from "./pages/Anuncio";
+import Login from "./pages/Login";
+import Perfil from "./pages/Perfil";
+import Registro from "./pages/Registro";
+import SubirAnuncio from "./pages/SubirAnuncio";
+
+import { AuthProvider } from "./shared/context/authContext";
+import MisAnuncios from "./components/pagina/MisAnuncios";
+import MisSolicitudes from "./components/pagina/MisSolicitudes";
+import MisReservas from "./components/pagina/MisReservas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+
+          <Route exact path="/acceder">
+            <PublicRoute>
+              <Login></Login>
+            </PublicRoute>
+          </Route>
+
+          <Route exact path="/registro">
+            <PublicRoute>
+              <Registro></Registro>
+            </PublicRoute>
+          </Route>
+
+          <Route exact path="/comprar">
+            <ListaDeCategorias></ListaDeCategorias>
+          </Route>
+
+          <Route exact path="/comprar/:idCategoria">
+            <ListaAnunciosCategoria></ListaAnunciosCategoria>
+          </Route>
+
+          <Route exact path="/comprar/:idCategoria/:idAnuncio">
+            <Anuncio></Anuncio>
+          </Route>
+
+          <Route exact path="/subir">
+            <PrivateRoute>
+              <SubirAnuncio></SubirAnuncio>
+            </PrivateRoute>
+          </Route>
+
+          <Route path="/admin">
+            <AdminRoute>
+              <Admin></Admin>
+            </AdminRoute>
+          </Route>
+
+          <Route exact path="/perfil">
+            <PrivateRoute>
+              <Perfil></Perfil>
+            </PrivateRoute>
+          </Route>
+
+          <Route exact path="/mis-anuncios">
+            <PrivateRoute>
+              <MisAnuncios></MisAnuncios>
+            </PrivateRoute>
+          </Route>
+
+          <Route exact path="/mis-solicitudes">
+            <PrivateRoute>
+              <MisSolicitudes></MisSolicitudes>
+            </PrivateRoute>
+          </Route>
+
+          <Route exact path="/mis-reservas">
+            <PrivateRoute>
+              <MisReservas></MisReservas>
+            </PrivateRoute>
+          </Route>
+        </Switch>
+        {/* <Footer></Footer> */}
+      </AuthProvider>
+    </Router>
   );
 }
 
